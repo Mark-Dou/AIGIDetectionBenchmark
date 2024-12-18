fe87a9089535e542dfc8a4b85744f0de5e4349d1
Mark-Dou <45621751+Mark-Dou@users.noreply.github.com>
2024-12-18 18:06:04 +0800

    Add files via upload

---
+++ bash_scripts/SAFE/CL_test_AA_AF.sh
@@ -0,0 +1,28 @@
+export PYTHONPATH=..:$PYTHONPATH
+
+####################################
+export CUDA_VISIBLE_DEVICES=3
+
+DATAROOT="CL_datasets/test"
+CHECKPOINT_NAME="progan,deepfake,biggan,stylegan2,ddpm,adm,dalle,glide,sdv4,midjourney,vqdm,sdv2.1,sdxl1.0,sdv3.0"
+DATASET_NAME="progan,cyclegan,stargan,deepfake,biggan,stylegan,gaugan,stylegan2,ddpm,adm,iddpm,dalle,glide,ldm,pndm,wukong,sdv1.4,midjourney,sdv1.5,vqdm,sdv2.1,sdxl1.0,sd-turbo,sdxl-turbo,sdv3.0,pixart,flux.1"
+NAME="SAFE"
+CL_METHODS=("Seq" "ER" "EWC" "OSLA" "AGEM" "SI" "iCaRL" "Linear")
+BS=256
+####################################
+
+cd ..
+
+for CL_METHOD in "${CL_METHODS[@]}"; do
+    echo "Running with CL method: $CL_METHOD"
+    python -W ignore script/CL_test_AA_AF.py \
+    --dataroot $DATAROOT \
+    --dataset_name $DATASET_NAME \
+    --multiclass 1 1 0 0 0 1 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 \
+    --detection_model $NAME \
+    --model_dir ./outputs/continual_learning \
+    --output_dir ./CL_results \
+    --checkpoint_name $ \
+    --cl_method $CL_METHOD \
+    --batch_size $BS
+done
---
+++ bash_scripts/SAFE/CL_train.sh
@@ -0,0 +1,29 @@
+export PYTHONPATH=..:$PYTHONPATH
+
+####################################
+export CUDA_VISIBLE_DEVICES=2
+
+DATAROOT="CL_datasets/train"
+DATASET_NAME="progan,deepfake,biggan,stylegan2,ddpm,adm,dalle,glide,sdv4,midjourney,vqdm,sdv2.1,sdxl1.0,sdv3.0"
+NAME="SAFE"
+WEIGHT_PATH="weights/classifier/SAFE/checkpoint-best.pth"
+CL_METHODS=("Seq" "ER" "EWC" "OSLA" "AGEM" "SI" "iCaRL" "Linear")
+BS=256
+####################################
+
+cd ..
+
+for CL_METHOD in "${CL_METHODS[@]}"; do
+    echo "Running with CL method: $CL_METHOD"
+    python -W ignore script/CL_train.py \
+    --dataroot $DATAROOT \
+    --dataset_name $DATASET_NAME \
+    --multiclass 1 0 0 1 0 0 0 0 0 0 0 0 0 0 \
+    --detection_model $NAME \
+    --initial_weight_path  $WEIGHT_PATH \
+    --per_device_train_batch_size $BS \
+    --per_device_eval_batch_size $BS \
+    --cl_method $CL_METHOD \
+    --skip_initial_training True \
+    --output_dir ./outputs/continual_learning/$NAME/$CL_METHOD
+done
