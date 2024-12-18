1380a47db31cff2383281eb1d8c73781b93b6788
Mark-Dou <45621751+Mark-Dou@users.noreply.github.com>
2024-12-18 13:50:08 +0800

    Add files via upload

---
+++ bash_scripts/LGrad/train.sh
@@ -0,0 +1,24 @@
+export PYTHONPATH=..:$PYTHONPATH
+
+####################################
+export CUDA_VISIBLE_DEVICES=4
+DDP_WORLD_SIZE=1
+DDP_PORT=12346
+
+DATAROOT="/dev/shm/why_data/ProGAN"
+CLASSES="airplane,bird,bicycle,boat,bottle,bus,car,cat,cow,chair,diningtable,dog,person,pottedplant,motorbike,tvmonitor,train,sheep,sofa,horse"
+NAME="LGrad"
+BS=8
+####################################
+
+cd ..
+python -W ignore script/train.py \
+--ddp_world_size $DDP_WORLD_SIZE \
+--ddp_port $DDP_PORT \
+--dataroot $DATAROOT \
+--classes $CLASSES \
+--name $NAME \
+--detection_model $NAME \
+--num_threads 8 \
+--optim "adamw" \
+--batch_size $BS
