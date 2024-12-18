46ebed41ed9e7598db4fb8c7a0388f63dae04ca4
Mark-Dou <45621751+Mark-Dou@users.noreply.github.com>
2024-12-18 18:07:22 +0800

    Add files via upload

--- bash_scripts/UnivFD/test.sh
+++ bash_scripts/UnivFD/test.sh
@@ -6,7 +6,7 @@ TESTSETS=("ProGAN" "LSUN-bedroom" "GenImage" "cvpr23_ojha" "DRCT-2M" "DiTFake")
 DEVICE="cuda:4"
 NAME="UnivFD"
 MODEL_PATH="weights/classifier/UnivFD/fc_weights.pth"
-BS=4
+BS=256
 
 cd ..
 
