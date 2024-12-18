b22673ac49490b561683b15aa09b0bf2829af09a
Mark-Dou <45621751+Mark-Dou@users.noreply.github.com>
2024-12-18 18:03:25 +0800

    Add files via upload

---
+++ bash_scripts/FreDect/CL_train.sh
@@ -0,0 +1,29 @@
+export PYTHONPATH=..:$PYTHONPATH
+
+####################################
+export CUDA_VISIBLE_DEVICES=3
+
+DATAROOT="CL_datasets/train"
+DATASET_NAME="progan,deepfake,biggan,stylegan2,ddpm,adm,dalle,glide,sdv4,midjourney,vqdm,sdv2.1,sdxl1.0,sdv3.0"
+NAME="FreDect"
+WEIGHT_PATH="weights/classifier/CNNSpot/blur_jpg_prob0.1.pth"
+CL_METHODS=("Seq" "ER" "EWC" "OSLA" "AGEM" "SI" "iCaRL" "Linear")
+BS=256
+####################################
+
+cd ..
+
+for CL_METHOD in "${CL_METHODS[@]}"; do
+    echo "Running with CL method: $CL_METHOD"
+    python -W ignore script/CL_train.py \
+    --dataroot $DATAROOT \
+    --dataset_name $DATASET_NAME \
+    --multiclass 1 0 0 1 0 0 0 0 0 0 0 0 0 0 \
+    --detection_model $NAME \
+    --initial_weight_path  $WEIGHT_PATH \
+    --per_device_train_batch_size $BS \
+    --per_device_eval_batch_size $BS \
+    --cl_method $CL_METHOD \
+    --skip_initial_training True \
+    --output_dir ./outputs/continual_learning/$NAME/$CL_METHOD
+done
--- bash_scripts/FreDect/test.sh
+++ bash_scripts/FreDect/test.sh
@@ -6,7 +6,7 @@ TESTSETS=("ProGAN" "LSUN-bedroom" "GenImage" "cvpr23_ojha" "DRCT-2M" "DiTFake")
 DEVICE="cuda:4"
 NAME="FreDect"
 MODEL_PATH="weights/classifier/FreDect/DCTAnalysis.pth"
-BS=32
+BS=256
 
 cd ..
 
